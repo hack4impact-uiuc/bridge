e1effc0e4518a481f65ad99f029fdf3ddc83c5ef
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-02 20:23:32 -0500

    Version bump to 0.0.7

--- src/components/Flex/Flex.js
+++ src/components/Flex/Flex.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 import { FLEX } from '../../utils/constants';
 import theme from '../../theme';
 import Box from '../Box';
