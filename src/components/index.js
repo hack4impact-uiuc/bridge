066ac6a8498ac36276d96a199407aca485d1c9ab
Jacqueline <jackieo5023@gmail.com>
2020-05-12 19:57:21 -0500

    Add tests

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,5 @@ export { default as Flex } from './Flex';
 export { default as Grid } from './Grid';
 export { default as Heading } from './Heading';
 export { default as Link } from './Link';
-export { default as Text } from './Text';
 export { default as Tag } from './Tag';
+export { default as Text } from './Text';
