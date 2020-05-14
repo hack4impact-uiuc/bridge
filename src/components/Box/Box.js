a88b1d5428b23b192216b1a75201d30503d16159
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-14 14:29:39 -0500

    update

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,25 +1,21 @@
 // taken from Github primer/components https://github.com/primer/components/blob/master/src/Box.js
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { space, color } from 'styled-system';
-import systemPropTypes from '@styled-system/prop-types';
-import { LAYOUT, FLEX } from '../../utils/constants';
+import { COMMON, FLEX, COLOR } from '../../utils/constants';
 import theme from '../../theme';
 
 const Box = styled.div`
-  ${LAYOUT}
-  ${FLEX}
-  ${space}
-  ${color}
+  ${COMMON};
+  ${FLEX};
+  ${COLOR};
 `;
 
 Box.defaultProps = { theme };
 
 Box.propTypes = {
-  ...LAYOUT.propTypes,
+  ...COMMON.propTypes,
   ...FLEX.propTypes,
-  ...systemPropTypes.space,
-  ...systemPropTypes.color,
+  ...COLOR.propTypes,
   theme: PropTypes.object,
 };
 
