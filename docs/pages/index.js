bcdda2d00e3347d0c1604f8a7cf6ca31f66fe8f6
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-14 01:27:08 -0500

    beginnings of home page

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
+import { Flex, Box } from '@hack4impact-uiuc/bridge';
+import BridgeLogo from '../public/bridge_full_logo.svg';
 
 // Home Page linking to
 // 1) component documentation
 // 2) design system documentation
 const HomePage = () => (
-  <div>
-    <h1 className="h1">Hack4Impact Design system</h1>
+  <Flex>
+    <Flex margin="auto" maxWidth="1200px" flexDirection="row" flexWrap="wrap">
+      <Box>
+        <h1 className="h1">Hack4Impact Design system</h1>
+      </Box>
+      <BridgeLogo width="300px" />
+    </Flex>
     <Link href="/components">Components</Link>
     <br />
     <Link href="/design">Design Principles</Link>
-  </div>
+  </Flex>
 );
 
 export default HomePage;
