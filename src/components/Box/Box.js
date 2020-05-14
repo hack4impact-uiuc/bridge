dcd688c64251d9fca1df7bb792a0481683ed268d
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-14 01:34:30 -0500

    Add Flex props to Box to allow it to be used as a flex item + bump to 0.0.4

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { space, color } from 'styled-system';
 import systemPropTypes from '@styled-system/prop-types';
-import { LAYOUT } from '../../utils/constants';
+import { LAYOUT, FLEX } from '../../utils/constants';
 import theme from '../../theme';
 
 const Box = styled.div`
   ${LAYOUT}
+  ${FLEX}
   ${space}
   ${color}
 `;
@@ -16,6 +17,7 @@ Box.defaultProps = { theme };
 
 Box.propTypes = {
   ...LAYOUT.propTypes,
+  ...FLEX.propTypes,
   ...systemPropTypes.space,
   ...systemPropTypes.color,
   theme: PropTypes.object,
