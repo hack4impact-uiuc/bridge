47baa7a094fa1707b226abe8a699e0abd9d37119
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-17 15:37:22 -0500

    boom

--- docs/components/sidebar.js
+++ docs/components/sidebar.js
@@ -4,18 +4,32 @@ import DesignDocsList from '../lib/data/designs';
 
 const getSidebarRoutes = (type) => {
   if (type === 'components') {
-    return ComponentDocsList.map((elm) => (
-      <Text as="p"><Link href={elm.href}>{elm.title}</Link></Text>
+    return ComponentDocsList.map((section) => (
+      <>
+        <Text type="body" fontWeight="700" as="p" m="5px 0">{section.name}</Text>
+        {
+        section.routes.map((elm) => (
+          <Text as="p" m="5px 0"><Link href={elm.href}>{elm.title}</Link></Text>
+        ))
+        }
+      </>
     ));
   }
   if (type === 'designs') {
-    return DesignDocsList.map((elm) => (
-      <Text as="p"><Link href={elm.href}>{elm.title}</Link></Text>
+    return DesignDocsList.map((section) => (
+      <>
+        <Text type="subtitle" as="p">{section.name}</Text>
+        {
+        section.routes.map((elm) => (
+          <Text as="p"><Link href={elm.href}>{elm.title}</Link></Text>
+        ))
+      }
+      </>
     ));
   }
+
   return null;
 };
-
 const Sidebar = ({ children, innerRef, type }) => (
   <Flex
     flexShrink="0"
