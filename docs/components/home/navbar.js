16cf39e683c0a3060035bd59094e9053bc63b6bc
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-21 23:11:50 -0500

    Resolves #73 #43 Home Page and navbar

---
+++ docs/components/home/navbar.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState, useEffect } from 'react';
+import { Flex, Link, Box } from '@hack4impact-uiuc/bridge';
+
+const NavBar = () => {
+  const [showNav, toggleNav] = useState(false);
+
+  const onToggle = () => {
+    toggleNav(!showNav);
+  };
+
+  const [windowDimensions, setWindowDimensions] = useState({ width: 1000, height: 1000 });
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    setWindowDimensions({ width: innerWidth, height: innerHeight });
+
+    function handleResize() {
+      const { innerWidth: widthResize, innerHeight: heightResize } = window;
+      setWindowDimensions({ width: widthResize, height: heightResize });
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  console.log(windowDimensions);
+  console.log(showNav);
+
+  return (
+
+    <nav>
+      <div className="navbar">
+        <span className="navbar-toggle" id="js-navbar-toggle" onClick={onToggle}>
+          <img src="/homepage/menu.svg" alt="hamburger menu" />
+        </span>
+        <Link ml="22px" href="https://bridge-ui.now.sh">
+          <img src="/bridge_full_logo.png" alt="bridge ui logo" width="128px" />
+        </Link>
+        <Box className="nav-links-box" display={showNav && windowDimensions.width <= 768 ? 'block' : windowDimensions.width > 768 ? 'flex' : 'none'}>
+          <ul className="nav-links">
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/design">Design Guidelines</Link>
+            </li>
+            <li>
+              <Link href="/components">Components</Link>
+            </li>
+            <li>
+              <Link href="https://github.com/hack4impact-uiuc/bridge">
+                <img width="24px" src="/homepage/github-logo.png" alt="github link logo" />
+              </Link>
+            </li>
+          </ul>
+        </Box>
+        <style jsx>
+          {`
+          .navbar {
+            max-width: 1000px;
+            margin: 20px;
+          }
+
+          .logo {
+            display: inline-block;
+          }
+
+          .navbar-toggle {
+              position: absolute;
+              top: 20px;
+              right: 20px;
+              cursor: pointer; 
+              color: rgba(255,255,255,0.8);
+              
+              height: 24px;
+              padding: 6px 6px;
+              border: 1px solid #EBEEF2;
+              border-radius: 8px;
+          }
+
+          .nav-links {
+            list-style-type: none;
+            margin: 15px auto;
+          }
+          
+          .nav-links li {
+            text-align: center;
+            margin: 15px;
+          }
+
+        @media screen and (min-width: 768px) {
+          .navbar {
+            display: flex;
+            justify-content: space-between;
+            padding-bottom: 0;
+            height: 70px;
+            align-items: center;
+            
+            max-width: 1000px;
+            margin: 12px auto;
+          }
+          
+          .nav-links {
+            margin: 0;
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-end;
+          }
+
+          .nav-links li {
+            text-align: center;
+            margin: 15px 40px;
+          }
+
+          .navbar-toggle {
+            display: none;
+        }
+        }
+
+      `}
+
+        </style>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,49 +1,97 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import Link from 'next/link';
 import {
-  Flex, Box, Heading, Text, Button,
+  Flex, Box, Heading, Text, Button, Link,
 } from '@hack4impact-uiuc/bridge';
-import BridgeLogo from '../public/bridge-logo.svg';
+import { useRouter } from 'next/router';
+
+import Row from '../components/row';
+import Col from '../components/col';
+import NavBar from '../components/home/navbar';
+import ComponentImage from '../public/homepage/components.svg';
 
 // Home Page linking to
 // 1) component documentation
 // 2) design system documentation
-const HomePage = () => (
-  <Box m="5px auto" maxWidth="1200px">
-    <Flex margin="30px auto" flexDirection="row" flexWrap="wrap">
-      <Box mr={[0, '20px']} flex={1}>
-        <Heading fontSize={[60, 84]} type="h1">Bridge</Heading>
-        <Text type="subtitle">A Hack4Impact UIUC Design System - simple and flexible</Text>
-      </Box>
-      <Box ml={[0, '40px']} flex={1}>
-        <Box minWidth="100px" maxWidth="250px">
-          <BridgeLogo />
-        </Box>
-      </Box>
-    </Flex>
-    <Flex mb="30px" flexDirection="row">
-      <Flex>
-        <Box flex={1} mr="30px">
-          <img src="/temp1.png" alt="description of design" width="100%" />
-        </Box>
-        <Box flex={1}>
-          <Heading type="h3">Our Interface Guidelines</Heading>
-          <Button as="a" href="/design">Design Principles</Button>
+const HomePage = () => {
+  const router = useRouter();
+  return (
+    <Flex minHeight="100vh" flexDirection="column">
+      <NavBar />
+      <Box flex={1} m={['0px 16px', '32px auto']} width="100%" maxWidth="1000px">
+        <Flex margin="40px auto" flexDirection="row" flexWrap="wrap">
+          <Box m={['0 8px', '0 20px 0 0']} flex={['0 0 100%', 1]} width="100%" order={[2, 1]}>
+            <Heading fontSize={[60, 84]} m={['12px 0 8px', '80px 0 20px']} type="h1">Bridge</Heading>
+            <Text maxWidth={['380px', '422px']} type="subtitle" as="p">
+              A design system built for Hack4Impact UIUC
+              to help us build great products for all kinds
+              of nonprofits.
+            </Text>
+            <Text type="body" as="p">
+              <Link href="/about">About</Link>
+              ・
+              <Link href="https://github.com/hack4impact-uiuc/bridge">Github</Link>
+              ・
+              <Link href="https://uiuc.hack4impact.org">Hack4Impact UIUC</Link>
+            </Text>
+          </Box>
+          <Box ml={[0, '40px']} flex={['0 0 100%', 1]} width="100%" order={[1, 2]}>
+            <Box minWidth="100px" maxWidth="400px" m="0 auto">
+              <img src="/homepage/bridge-web-landing.svg" alt="bridge hero" width="100%" />
+            </Box>
+          </Box>
+        </Flex>
+        <Box m={['56px auto 12px auto', '56px auto']} p={['24px 0', '44px 0 56px']} maxWidth="800px">
+          <Heading textAlign="center" type="h3">Design Resources</Heading>
+          <Row>
+            <Col m={['24px 0', '0 48px']}>
+              <Box width="100%">
+                <ComponentImage />
+                <Box m={['0 8px']}>
+                  <Text type="overline" as="p">Design</Text>
+                  <Text type="body" as="p">
+                    Design principles, UI Patterns, and usage guidelines
+                    for creating products with Bridge.
+                  </Text>
+                  <Link as="button" onClick={() => router.push('/design')}>View Design Principles</Link>
+                </Box>
+              </Box>
+            </Col>
+            <Col m={['24px 0', '0 48px']}>
+              <Box width="100%">
 
+                <img src="/homepage/design.svg" alt="description of react component library" width="100%" />
+                <Box m={['0 8px']}>
+                  <Text type="overline" as="p">React Components</Text>
+                  <Text type="body" as="p">
+                    Quality components with theme-based props that serve
+                    as building blocks for building products.
+                  </Text>
+                  <Link as="button" onClick={() => router.push('/components')}>Browse Components Library</Link>
+                </Box>
+              </Box>
+            </Col>
+          </Row>
         </Box>
-      </Flex>
-    </Flex>
-    <Flex mt="30px" flexDirection="row">
-      <Box flex={1}>
-        <Heading type="h3">Our React Component library</Heading>
-        <Button as="a" href="/components">Components</Button>
       </Box>
-      <Box flex={1} ml="30px">
-        <img src="/temp2.png" alt="description of react component library" width="100%" />
-      </Box>
+      <footer className="footer">
+        <Flex alignItem="center" bg="indigoPalette.dark" padding={['24px 0 20px 32px', '24px 0 20px 72px']} width="100%">
+          <Box>
+            <Link href="https://uiuc.hack4impact.org"><img src="/homepage/white_h4i_logo.png" width="150px" alt="hack4impact uiuc logo" /></Link>
+          </Box>
+        </Flex>
+      </footer>
+      <style jsx>
+        {`
+      .footer {
+        margin-top: 52px;
+      }
+    
+    `}
 
+      </style>
     </Flex>
-  </Box>
-);
+  );
+};
 
 export default HomePage;
