f1f56996dd9c6f7c1376bc20c58eeeee45a5db0f
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-29 21:21:00 -0700

    Add more design guidelines. Alert and spacing documentation. Alert stuff

--- docs/lib/data/design.js
+++ docs/lib/data/design.js
@@ -12,25 +12,57 @@ export default [
   },
   {
     name: 'Base',
-    routes: [{
-      title: 'Colors',
-      href: `${DESIGN_URL}/colors`,
-    },
-    {
-      title: 'Typography',
-      href: `${DESIGN_URL}/typography`,
-    },
+    routes: [
+      {
+        title: 'Colors',
+        href: `${DESIGN_URL}/colors`,
+      },
+      {
+        title: 'Typography',
+        href: `${DESIGN_URL}/typography`,
+      },
+      {
+        title: 'Logos',
+        href: `${DESIGN_URL}/logos`,
+      },
+      {
+        title: 'Spacing',
+        href: `${DESIGN_URL}/spacing`,
+      },
+      {
+        title: 'Breakpoints',
+        href: `${DESIGN_URL}/breakpoints`,
+      },
     ],
   },
   {
     name: 'UI Patterns',
-    routes: [{
-      title: 'Buttons',
-      href: `${DESIGN_URL}/buttons`,
-    },
-    {
-      title: 'Alerts',
-      href: `${DESIGN_URL}/alerts`,
-    }],
+    routes: [
+      {
+        title: 'Alerts',
+        href: `${DESIGN_URL}/alerts`,
+      },
+      {
+        title: 'Buttons',
+        href: `${DESIGN_URL}/buttons`,
+      },
+      {
+        title: 'Cards',
+        href: `${DESIGN_URL}/cards`,
+      },
+      {
+        title: 'Forms',
+        href: `${DESIGN_URL}/forms`,
+      },
+      {
+        title: 'Tags',
+        href: `${DESIGN_URL}/tags`,
+      },
+      {
+        title: 'Tables',
+        href: `${DESIGN_URL}/tables`,
+      },
+
+    ],
   },
 ];
